2770aa190c44d71e79b7bf0d6aed996ec78ad243
Paingthukha <149249788+ptkakaeitoe@users.noreply.github.com>
2024-12-28 01:37:28 +0700

    no idea

--- api/passcode.js
+++ api/passcode.js
@@ -1,10 +1,7 @@
-const express = require("express");
-const app = express();
-
-const PASSCODE = process.env.PASSCODE || "2516";
-
-app.get("/", (req, res) => {
-  res.json({ passcode: PASSCODE });
-});
-
-module.exports = app;
+module.exports = (req, res) => {
+  if (req.method === "GET") {
+    res.status(200).json({ passcode: "2516" });
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
+};
